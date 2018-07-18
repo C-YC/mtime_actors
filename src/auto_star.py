cdccb4a1a3c1f096a0e8a51f2ee89940aec50c7d
C-YC <39453919+C-YC@users.noreply.github.com>
2018-07-18 19:53:32 +0800

    Update auto_star.py

--- src/auto_star.py
+++ src/auto_star.py
@@ -10,5 +10,6 @@ if __name__ == '__main__':
         try_time = try_time+1
         time.sleep(1)
         print "第"+str(try_time)+"次尝试"
+        time.sleep(3)
         os.system('python2.7 ./get_actor_works.py')
     print "第"+str(try_time)+"次尝试","任务结束"
